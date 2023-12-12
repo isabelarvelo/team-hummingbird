7b4b28b64968c8ac6904de558e3d8bfd9b02dc0f
Sankhya <sankhyasiva16@gmail.com>
2023-12-12 23:16:50 +0000

    Modified the NB and UI

--- app.py
+++ app.py
@@ -1,44 +1,68 @@
 import streamlit as st
-import pandas as pd
-import time
 from ai_assisted_coding_final.AskTheDataset import *
+import time
 
-st.title("CSV and Chatbot Interface")
+st.title("Ask about the Dataset Page")
 
 MAX_HISTORY_LENGTH = 100  # Maximum number of messages in chat history
+
+# Welcome Message and Instructions
+st.markdown("""
+Welcome to the Data Analysis Chatbot! 
+
+This interactive tool allows you to upload your CSV data files and ask questions about your data using natural language. 
+Our AI-powered assistant will help you understand and analyze your data.
+
+**Follow these simple steps to get started:**
+1. **Enter your OpenAI API Key**: This is required to enable the AI-powered analysis. You can sign up for an account and register for an API key at https://openai.com/blog/openai-api
+2. **Upload Your CSV Data**: You can upload one or more CSV files containing your data.
+3. **Ask Questions**: Once your data is uploaded, simply type in your questions about the data in the chat interface. The AI assistant will respond with insights and analyses.
+
+At any point during the chat, if you want to upload new data, you can upload new CSV files/ remove existing ones and the AI assistant will use the new data for analysis.
+
+You can also clear the chat history at any point by clicking the "Clear Chat History" button.
+
+Let's begin!
+""")
 
 # Initialize session state
 if 'history' not in st.session_state:
     st.session_state.history = []
 if 'api_key' not in st.session_state:
     st.session_state.api_key = None
-if 'df' not in st.session_state:
+if 'data_manager' not in st.session_state:
+    st.session_state.data_manager = CSVFileManager()
+if 'ai_handler' not in st.session_state:
+    st.session_state.ai_handler = None
+if 'df' not in st.session_state:  # Initialize 'df' in session state
     st.session_state.df = None
 
-# Function to handle CSV files
-def process_csv(uploaded_files):
-    try:
-        if uploaded_files is not None:
-            df = pd.concat([pd.read_csv(file) for file in uploaded_files])
-            return df
-        return None
-    except:
-        st.error("Please upload valid CSV files")
+# Function to clear chat history
+def clear_history():
+    st.session_state.history = []
 
+    
 # API Key Input
 if not st.session_state.api_key:
-    st.session_state.api_key = st.text_input("Enter your OpenAI API Key", type="password")
+    api_key_input = st.text_input("Enter your OpenAI API Key", type="password")
+    if st.button('Submit'):
+        st.session_state.api_key = api_key_input
+        if 'ai_handler' not in st.session_state or st.session_state.ai_handler is None:
+            st.session_state.ai_handler = GPTQuestionsHandler(api_key_input)
+        st.write("Please proceed to upload your CSV files")
 
-# File Uploader - Always available
+# File Uploader
 uploaded_files = st.file_uploader("Upload CSV files", accept_multiple_files=True)
 if uploaded_files:
-    st.session_state.df = process_csv(uploaded_files)
+    st.session_state.data_manager.load_data(uploaded_files)
+    st.session_state.df = st.session_state.data_manager.data_frame
+
 
 # Function to process user query
 def process_query(user_query):
-    if st.session_state.api_key and st.session_state.df is not None and user_query:
+    if st.session_state.ai_handler and st.session_state.df is not None and user_query:
         csv_combined = st.session_state.df.to_string()
-        response = ask_gpt(user_query, csv_combined, st.session_state.api_key)
+        response = st.session_state.ai_handler.ask_gpt(user_query, csv_combined)
         
         # Simulate typing for assistant response
         with st.chat_message("assistant"):
@@ -64,6 +88,7 @@ def display_history():
 
 display_history()
 
+
 # Chat Interface
 if st.session_state.api_key and st.session_state.df is not None:
     user_query = st.chat_input("Ask a question about the dataset")
@@ -79,3 +104,6 @@ if st.session_state.api_key and st.session_state.df is not None:
         
         # Process and respond to query
         process_query(user_query)
+
+if st.button('Clear Chat History'):
+    clear_history()
